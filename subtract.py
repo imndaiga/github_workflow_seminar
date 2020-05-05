35aaa56c2c76e5daacbc8ebfc76f0e26759228d0
davidjohnbaker1 <davidjohnbaker1@gmail.com>
2020-05-05 11:39:44 +0100

    subtraction function created

--- subtract.py
+++ subtract.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 
+# get two numbers 
+x = eval(input('Tell me your first number:'))
+y = eval(input('Tell me your second number:'))
+
+answer = float(x) - float(y)
+
+print(answer)
 
