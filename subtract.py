2fd8ed716c6f588a041e795b704202e19e6519ce
davidjohnbaker1 <davidjohnbaker1@gmail.com>
2020-05-11 14:26:09 +0100

    attempt 2 subtract

--- subtract.py
+++ subtract.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 
-# get two numbers 
-x = eval(input('Tell me your first number:'))
-y = eval(input('Tell me your second number:'))
-
-answer = float(x) - float(y)
-
-print(answer)
-
+def subtract(x,y):
+    answer = float(x) - float(y)
+    return(answer)
